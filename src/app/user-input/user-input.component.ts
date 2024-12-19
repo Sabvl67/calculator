4b289781f7e88d07c092501dd060ad25ba451140
VO Cindy -DARLINGTON <cindy.vo@opg.com>
2024-12-19 07:41:10 -0500

    calculator finish

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+// import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -9,7 +11,22 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  // @Output() calculate = new EventEmitter <InvestmentInput>();
+  enteredInitialInvestment = '0';
+  enteredAnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  constructor( public investmentService: InvestmentService ) {}
+
   onSubmit() {
-    console.log("submit")
+    this.investmentService.calculateInvestmentResults(
+      {
+        // convert into number
+      initialInvestment: +this.enteredInitialInvestment, 
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredInitialInvestment
+    });
   }
 }
